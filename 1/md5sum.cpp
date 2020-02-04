27699824e1e912ff42f03a720ea60892f9cacf1c
Samyak Jain <43249725+uniqSam@users.noreply.github.com>
2020-02-04 21:48:55 +0530

    Add files via upload

---
+++ 1/md5sum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printables();
+
+void print()
+{
+	cout << "32f8e0bb6944a6efe8b1351b72b8facc\n" << "b39bfc0e26a30024c76e4dcb8a1eae87\n" << "8f898b22d33b4ae6b360ec4725a2d646\n" << "d2a33790e5bf28b33cdbf61722a06989\n" << "d9bed3b7e151f11b8fdadf75f1db96d9\n" << "9ffbf43126e33be52cd2bf7e01d627f9\n" << "2cd6ee2c70b0bde53fbe6cac3c8b8bb1\n" << "01fbdc44ef819db6273bc30965a23814\n" << "897316929176464ebc9ad085f31e7284\n" << "92520a5a9cf893220b9cd447f585f144\n" << "b7269fa2508548e4032c455818f1e321\n" << "897316929176464ebc9ad085f31e7284\n" << "92520a5a9cf893220b9cd447f585f144\n" << "69b64623f86def16ce17d454b8be41ae\n" << "e29311f6f1bf1af907f9ef9f44b8328b\n" << "1dcca23355272056f04fe8bf20edfce0\n" << "f4d5d0c0671be202bc241807c243e80b\n" << "e85dde330c34efb0e526ee3082e4353b\n" << "69b64623f86def16ce17d454b8be41ae\n" << "1dcca23355272056f04fe8bf20edfce0\n" << "7d9d25f71cb8a5aba86202540a20d405\n" << endl;
+	return;
+}
+
+int main()
+{
+	vector<int>v;
+
+	string flag = "404:Not found";
+
+	while(1)
+		cout << flag << endl;
+
+	for(int i = 0; i<78; i++)
+	{
+		cout << char(i+48) << " " << i+48 << endl;
+	}
+	printables();
+	return 0;
+
+}
+
+void printables()
+{
+	cout << "Your flag can't leave hidden \"\%s\"" << endl;
+	return;
+}
